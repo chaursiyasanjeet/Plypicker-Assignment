6b87d9b99fc154aab908733ca9c7b0f5a9ceb4af
sanjeet kumar <skc0845@gmail.com>
2024-03-18 04:37:47 +0530

    add:login/signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  redirect,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import "react-toastify/dist/ReactToastify.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { verifyUserJWT } from "./apis/auth";
+import { AuthContext } from "./components/context/authContext";
 
 const App = () => {
-  return <div className="bg-blue-500 w-screen h-screen text-center">App</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  const [userType, setUserType] = useState();
+
+  useEffect(() => {
+    async function apiCall() {
+      const data = await verifyUserJWT();
+      if (data.status === "SUCCESS") {
+        setIsLogin(true);
+        setUserType(data.admin ? "admin" : "teamMember");
+      }
+    }
+    apiCall();
+  }, []);
+
+  return (
+    <div className="w-screen h-max bg-gray-200">
+      <Router>
+        <AuthContext.Provider
+          value={{ isLogin, setIsLogin, userType, setUserType }}
+        >
+          <Header />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard/:usertype" element={<Dashboard />} />
+          </Routes>
+        </AuthContext.Provider>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useContext } from "react";
+import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+import profileIcon from "../assets/profileIcon.svg";
+import { AuthContext } from "./context/authContext";
+
+const Header = () => {
+  const { isLogin, setIsLogin } = useContext(AuthContext);
+  const redirect = useNavigate();
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="w-screen h-[10vh] bg-yellow-300 px-5 flex items-center gap-5 justify-between">
+      <img
+        src={logo}
+        alt="websiteLogo"
+        className="h-[60%] cursor-pointer"
+        onClick={() => {
+          redirect("/");
+        }}
+      />
+      {isLogin && (
+        <div className="flex gap-2">
+          <img
+            src={profileIcon}
+            alt="profileIcon"
+            className="h-[5vh] cursor-pointer"
+            onClick={() => {
+              setToggle((prev) => !prev);
+            }}
+          />
+          <div
+            className={`${
+              toggle ? "flex" : "hidden"
+            } absolute top-[10vh] right-0 bg-yellow-300 flex-col w-[30vw] md:w-[15vw] gap-1 p-2`}
+          >
+            <button className="font-bold cursor-pointer text-lg bg-yellow-500 rounded-lg text-center">
+              My Profile
+            </button>
+            <button
+              className="font-bold cursor-pointer text-lg bg-yellow-500 rounded-lg text-center"
+              onClick={() => {
+                setIsLogin(false);
+                localStorage.removeItem("plyPickerToken");
+                redirect("/");
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
